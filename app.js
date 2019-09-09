7111baf23b786961695e47455d6dd7925f07d0f4
Nikolas9896 <nhlynka@gmail.com>
2019-09-09 18:40:32 +0300

    add RESTful router for training

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+var app = express();
+
+// "/" "Hi there!";
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+    console.log("Somebody make a request!YES YES!");
+    res.send("MEOW!");
+});
+app.listen(3000, function() { 
+    console.log('Server listening on port 3000'); 
+  });
+// "/bye" "Goodbye!";
+// "/dog" "MEOW!";
+
